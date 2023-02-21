0e397d7badc61cd9e36b55bbce1536d5b2d0d39a
marcopiraccini <marco.piraccini@gmail.com>
2023-02-21 15:15:59 +0100

    test pr-1

--- plugin.ts
+++
@@ -1,8 +0,0 @@
-/// <reference path="./global.d.ts" />
-import { FastifyInstance } from 'fastify'
-
-export default async function(app: FastifyInstance) {
-  app.get('/test', async function(request, response) {
-    return "hello world"
-  })
-}
